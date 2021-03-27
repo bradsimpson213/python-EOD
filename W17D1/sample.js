c4c5d112d073f4208d5f16f8641c08d2ccf6c609
bradsimpson213 <bsimpson@appacademy.io>
2021-03-26 22:25:41 -0400

    example 1

---
+++ W17D1/sample.js
@@ -0,0 +1,10 @@
+/* Ternary in JS VS Python
+Random Module in JS VS Python
+Readline in JS VS Input in Python
+File Reading with fs VS Open in Python
+Route in express VS Route in Flask */
+
+// Example #1 Ternary Operator
+let isGood = true;
+const dragon = isGood ? "Toothless" : "Smaug";
+console.log(dragon);
---
+++ W17D1/sample.py
@@ -0,0 +1,4 @@
+# Example #1 Ternary Operator
+is_good = True
+dragon = "Tootless" if is_good else "Smaug"
+print(dragon)

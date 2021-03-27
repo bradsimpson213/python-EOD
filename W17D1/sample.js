262137c077d987f29ea7579435a2cb35d0ca92b0
bradsimpson213 <bsimpson@appacademy.io>
2021-03-26 22:43:21 -0400

    example 2 completed

--- W17D1/sample.js
+++ W17D1/sample.js
@@ -1,10 +1,14 @@
-/* Ternary in JS VS Python
-Random Module in JS VS Python
-Readline in JS VS Input in Python
+/* Readline in JS VS Input in Python
 File Reading with fs VS Open in Python
 Route in express VS Route in Flask */
 
 // Example #1 Ternary Operator
-let isGood = true;
-const dragon = isGood ? "Toothless" : "Smaug";
-console.log(dragon);
+// let isGood = true;
+// const dragon = isGood ? "Toothless" : "Smaug";
+// console.log(dragon);
+
+// Example #2 Random Numbers
+const dragons = ['Puff', 'Toothless', 'Falkor', 'Draco'];
+const randNum = Math.floor(Math.random() * dragons.length);
+const dragonPick = dragons[randNum];
+console.log(dragonPick);
--- W17D1/sample.py
+++ W17D1/sample.py
@@ -1,4 +1,11 @@
 # Example #1 Ternary Operator
-is_good = True
-dragon = "Tootless" if is_good else "Smaug"
-print(dragon)
+# is_good = True
+# dragon = "Tootless" if is_good else "Smaug"
+# print(dragon)
+
+#example #2 Random Numbers
+import random
+dragons = ['Puff', 'Toothless', 'Falkor', 'Draco']
+dragonPick = dragons[random.randint(0, len(dragons) - 1)]
+# dragonPick = random.choice(dragons)
+print(dragonPick)

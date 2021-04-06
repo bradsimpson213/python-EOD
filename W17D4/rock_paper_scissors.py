e7505fe6411d4a67aa00476faf53dcaaf70858b8
bradsimpson213 <bsimpson@appacademy.io>
2021-04-05 22:11:07 -0400

    working on classes EOD

--- W17D4/rock_paper_scissors.py
+++ W17D4/rock_paper_scissors.py
@@ -1,7 +1,7 @@
 import random
 
 class RockPaperScissors:
-    def __init__(self, player="player 1" )
+    def __init__(self, player="player 1"):
         self.player = player
         self.wins = 0
         self.ties = 0
@@ -10,11 +10,22 @@ class RockPaperScissors:
         self.player_choice = ''
         self.computer_choice = ''
 
-    
-    def computer_move (self):
-        self.computer_choice = random.choices(self.choices)
+    def decide_winner(self):
+        if self.player_choice == self.computer_choice:
+            print(f'{self.player} and the Computer both chose {self.player_choice}, its a Tie!')
+            return
+        if ((self.player_choice == 'rock' and self.computer_choice == 'scissors')
+             or (self.player_choice == 'paper' and self.computer_choice == 'rock')
+             or (self.player_choice == 'scissors' and self.computer_choice == 'paper')):
+            print(f'{self.player} played "{self.player_choice}" and the Computer played "{self.computer_choice}", You Win!')
+        else:
+            print(f'{self.player} played "{self.player_choice}" and the Computer played "{self.computer_choice}", You Lose!')
 
-    def player_move (self):
+    def computer_move(self):
+        self.computer_choice = random.choice(self.choices)
+        self.decide_winner()
+
+    def player_move(self):
         play = True
         while play:
             player_input = input('Choose your move ( rock, paper, or scissors): ')
@@ -22,10 +33,14 @@ class RockPaperScissors:
                 self.player_choice = player_input
                 play = False
             else:
-                print(f'{player_input} is not a valid move, please try again')
+                print(f'"{player_input}" is not a valid move, please try again')
         self.computer_move()
         
 
+
+
+play_game = RockPaperScissors('Brad')
+play_game.player_move()
    
             
 

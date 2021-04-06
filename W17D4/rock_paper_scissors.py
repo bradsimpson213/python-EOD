3d3560de55fb41ddbcd20ff333aa1432ae283ace
bradsimpson213 <bsimpson@appacademy.io>
2021-04-05 22:22:47 -0400

    working on classes EOD

--- W17D4/rock_paper_scissors.py
+++ W17D4/rock_paper_scissors.py
@@ -10,20 +10,37 @@ class RockPaperScissors:
         self.player_choice = ''
         self.computer_choice = ''
 
+
+    def report_score(self):
+        print(f'Current score is {self.wins} Wins, {self.ties} Ties, and {self.losses} Losses...')
+        play_again = input("Would you like to play again (yes or no)?: ")
+        if play_again == 'yes':
+            self.player_move()
+        else:
+            print(f"{self.player}'s final score was {self.wins} Wins, {self.ties} Ties, and {self.losses} Losses")
+
+
     def decide_winner(self):
         if self.player_choice == self.computer_choice:
-            print(f'{self.player} and the Computer both chose {self.player_choice}, its a Tie!')
+            print(f'{self.player} and the Computer both chose "{self.player_choice}", its a Tie!')
+            self.ties +=1
+            self.report_score()
             return
         if ((self.player_choice == 'rock' and self.computer_choice == 'scissors')
-             or (self.player_choice == 'paper' and self.computer_choice == 'rock')
-             or (self.player_choice == 'scissors' and self.computer_choice == 'paper')):
+            or (self.player_choice == 'paper' and self.computer_choice == 'rock')
+            or (self.player_choice == 'scissors' and self.computer_choice == 'paper')):
             print(f'{self.player} played "{self.player_choice}" and the Computer played "{self.computer_choice}", You Win!')
+            self.wins +=1
         else:
             print(f'{self.player} played "{self.player_choice}" and the Computer played "{self.computer_choice}", You Lose!')
+            self.losses +=1
+        self.report_score()
+
 
     def computer_move(self):
         self.computer_choice = random.choice(self.choices)
         self.decide_winner()
+
 
     def player_move(self):
         play = True

1615efc82a40cb898294d8c5a10605b9c68b683e
Brad Simpson <bradsimpson@icloud.com>
2021-04-13 22:39:45 -0400

    made mew files in D4

---
+++ W17D4/r_p_s_starter.py
@@ -0,0 +1,15 @@
+class RockPaperScissors:
+    def __init__(self):
+    
+
+    def report_score(self):
+ 
+
+    def decide_winner(self):
+
+
+    def computer_move(self):
+    
+
+    def player_move(self):
+     

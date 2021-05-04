fbb4aa76fdd0d521366d8d9627d109627fd0731b
Brad Simpson <bradsimpson@icloud.com>
2021-05-04 18:12:01 -0400

    working on D2

--- W17D2/getting_help.py
+++ W17D2/getting_help.py
@@ -2,21 +2,44 @@
 
 num1 = 245
 num2 = 34.56
-boolean = True
 word = "Pizza"
 letters= ['a', 'b', 'c']
 
+help()
 # help(num1)
 # help(num2)
-# help(boolean)
-
+# help(word)
+# help(str)
 # help(letters)
 
 # Doc strings
 
 # def double(num):
-#     '''function that takes in a number as an arguement and return 2 times its value'''
+#     """
+#     function that takes in a number as an arguement and return 2 times its value
+#     """
 #     return num * 2
 
 
 # help(double)
+
+# class Book:
+    # """
+    # Class definition to create Book objects
+    # """
+    
+#     def __init__(self, title, series, author):
+#         """Book Class initializor"""
+#         self.title = title
+#         self.series = series
+#         self.author = author
+
+
+#     def get_information(self):
+#         '''Returns the author and title of the Book instance '''
+#         return f'{self.title} by {self.author}'
+
+
+# help(Book)
+
+

7c9d1e11cd65001fce31d8054c8791d4ed1a091d
Brad Simpson <bradsimpson@icloud.com>
2021-05-06 11:18:21 -0400

    changed order of methods

--- W17D4/r_p_s_starter.py
+++ W17D4/r_p_s_starter.py
@@ -1,15 +1,14 @@
 class RockPaperScissors:
     def __init__(self):
     
+    
+    def player_move(self):
 
-    def report_score(self):
- 
+
+    def computer_move(self):
+
 
     def decide_winner(self):
 
 
-    def computer_move(self):
-    
-
-    def player_move(self):
-     
+    def report_score(self):

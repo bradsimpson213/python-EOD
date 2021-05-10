c6b6a87b7298eadbfc8a64df314954c9b93953f4
Brad Simpson <bradsimpson@icloud.com>
2021-05-09 23:16:17 -0400

    working on a simple flask stuff

--- W18D1/flask-demo/app.py
+++ W18D1/flask-demo/app.py
@@ -1,6 +1,10 @@
-from flask import Flask
+from flask import (Flask, render_template)
 
 app = Flask(__name__)
+
+# @app.before_request
+# def before_request_function():
+#     print("Yoooo, you hit a route!")
 
 
 @app.route('/')
@@ -8,6 +12,17 @@ def hello():
     return f"<h1>What's good friends?</h1>"
 
 
+@app.route('/hello/<name>')
+def greeting(name):
+    return f"<h1>How's it going {name}???</h1>"
+
+
 @app.route('/sup')
+@app.route('/yo_dude')
 def sup_dude():
     return render_template('index.html')
+
+
+# @app.route('')
+# def lost():
+#     return f"<h1>You lost buddy!</h1>"

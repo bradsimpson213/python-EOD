b0a1cf818dd504f5fbd080e2d8f94f2e012cb34b
Brad Simpson <bradsimpson@icloud.com>
2021-05-10 19:04:24 -0400

    added to W18D1 EOD

--- W18D1/flask-demo/app.py
+++ W18D1/flask-demo/app.py
@@ -3,7 +3,6 @@ from flask import (Flask, render_template)
 app = Flask(__name__)
 
 
-
 @app.route('/')
 def hello():
     return f"<h1>What's good friends?</h1>"
@@ -19,3 +18,10 @@ def greeting(name):
 def sup_dude():
     return render_template('index.html')
 
+
+@app.route('/eod/<int:id>')
+def eod_time(id):
+    if id < 15:
+        return render_template('eod.html', start_time="7:15PM EST")
+    else:
+        return render_template('eod.html', start_time="7:30PM EST")

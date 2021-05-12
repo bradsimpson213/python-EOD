fbb1972fbe966531a441c889b9eda689f2cfdc28
Brad Simpson <bradsimpson@icloud.com>
2021-05-11 23:53:17 -0400

    working on D3 EOD

---
+++ W18D3/dad_jokes/app/__init__.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template, redirect
+from .config import Config
+from data import jokes
+from .joke_form import JokeForm
+import random
+# new imports
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+
+app.config.from_object(Config)
+# new create Sqlalchemy object
+db = SQLAlchemy(app)
+
+
+@app.route('/')
+def home():
+    joke = random.choice(jokes)
+    return render_template('index.html', page="Home", joke=joke)
+
+
+@app.route('/jokes')
+def all_jokes():
+    return render_template('alljokes.html', page="All Jokes", jokes=jokes)
+
+
+@app.route('/addjoke', methods=['GET', 'POST'])
+def add_joke():
+    form = JokeForm()
+
+    if form.validate_on_submit():
+        new_id = len(jokes) + 1
+        new_joke = {
+            'jokeid': new_id,
+            'joke': form.data['joke'],
+            'punchline': form.data['punchline'],
+            'rating': form.data['rating']
+        }
+        jokes.append(new_joke)
+        return redirect('/jokes')
+
+    return render_template('jokeform.html', form=form, errors=form.errors)
---
+++ W18D3/readme.md
@@ -0,0 +1,9 @@
+For D3 EOD
+
+make and connect DB
+
+Set up jokes model
+
+refactor routes to query db and create new objects
+
+create seed data (maybe for D4)
